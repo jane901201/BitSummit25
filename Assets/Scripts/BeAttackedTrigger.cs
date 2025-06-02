65b1cf5c281aa46b387a721856c5db1a5e2d9b5f
jane901201 <75302011+jane901201@users.noreply.github.com>
2025-06-02 19:39:50 +0900

    プレイヤーのゲームパッド操作を実装

--- Assets/Scripts/BeAttackedTrigger.cs
+++ Assets/Scripts/BeAttackedTrigger.cs
@@ -7,12 +7,10 @@ public class BeAttackedTrigger : MonoBehaviour
     {
         int damage = other.GetComponent<IGhost>()?.GetAttackPower() ?? 0;
         GameManager.Instance.TakeDamage(damage);
-        Debug.Log("Is be attacked");
     }
     
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("Destroy");
         Destroy(other.gameObject);
     }
 }
