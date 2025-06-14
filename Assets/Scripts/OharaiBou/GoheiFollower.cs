69d7665216b852edd399401e1d538b03ec0f8d55
surumisugita <seguti.kirima@gmail.com>
2025-06-14 16:54:52 +0900

    プレイヤーの移動、いらんマテリアル消去

--- Assets/Scripts/OharaiBou/GoheiFollower.cs
+++ Assets/Scripts/OharaiBou/GoheiFollower.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class GoheiFollower : MonoBehaviour
 {
     public Transform cursorTransform; // 追従先（JoystickDrawTrackerのオブジェクト）
+    public Transform masterTransform; // マスターな追従先
 
     void Update()
     {
@@ -10,18 +11,19 @@ public class GoheiFollower : MonoBehaviour
         {
             Vector3 currentPosition = transform.position;
             Vector3 cursorPos = cursorTransform.position;
+            Vector3 masterPos = masterTransform.position;
 
             transform.position = new Vector3(cursorPos.x/5, currentPosition.y, currentPosition.z);
             // カーソル方向へのベクトルを取得
             Vector3 direction;
-            direction.y = cursorPos.y - 10;
-            direction.x = cursorPos.x - 0;
+            direction.y = cursorPos.y - masterPos.y - 10;
+            direction.x = cursorPos.x - masterPos.x;
 
             // 回転角度を求める（2D用にZ軸周りの回転）
             float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
             // 回転を適用（Z軸のみ回転）
-            transform.rotation = Quaternion.Euler(0, 0, - angle - 90);
+            transform.rotation = masterTransform.rotation * Quaternion.Euler(0, 0, -angle - 90);
         }
     }
 }
